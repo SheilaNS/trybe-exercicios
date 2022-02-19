33aceddac3ee7f869d7752922c2e0a85d7ca68fc
SheilaNS <shei.nsantos@gmail.com>
2022-02-19 10:49:03 -0300

    Continuando os estudos

--- front-end/bloco-10-introducao-react/dia-02-componentes-react/conteudo/table/src/Table.js
+++ front-end/bloco-10-introducao-react/dia-02-componentes-react/conteudo/table/src/Table.js
@@ -19,8 +19,8 @@ class Table extends Component {
       },
       {
         id: 42,
-        name: "Amélia",
-        email: "amelia@gmail.com",
+        name: "Lucas Ferraz",
+        email: "lucas@gmail.com",
         avatar: "https:\/\/cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png",
       },
     ];
